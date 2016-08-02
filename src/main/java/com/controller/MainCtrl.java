52d4c50cf2e71bffcacfc56fb7a792dd5d123fae
Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>
2016-08-02 22:26:50 +0200

    * Adds an api to add a new user.

--- src/main/java/com/controller/MainCtrl.java
+++ src/main/java/com/controller/MainCtrl.java
@@ -79,16 +79,6 @@ public class MainCtrl {
         return UserMapper.registerUsersToUser(user);
     }
 
-    @RequestMapping(path="/auctions_list", method = RequestMethod.GET, produces = "application/json")
-    public List<ItemDto> auctions_list() throws Exception {
-        System.out.println("list  items");
-        List<Item> items = itemRepository.findByStartDate(null);
-        System.out.println(items);
-        // TODO: We need to add the list of categories associated with every item somehow somewhere. Maybe even in a previeous step (fetch it automaticaly using a `find` function)
-
-        return convertToItemDTOs(items);
-    }
-
     @RequestMapping(path = "/login", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
     @ExceptionHandler({BadRequestException.class})
     public UserLogInResponseDto login(@RequestBody UserLoginRequestDto userLogInRequestDto) throws Exception {
@@ -128,9 +118,55 @@ public class MainCtrl {
         UserSignUpResponseDto userSignUpResponseDto = new UserSignUpResponseDto();
         userSignUpResponseDto.setUserId(i);
 
-        return userSignUpResponseDto; // TODO: Return something meaningfull.
+        return userSignUpResponseDto; // TODO: Return something meaningful.
 
     }
+
+
+
+
+
+
+
+
+    // ITEMS
+    // TODO: MOVE TO NEW FILE
+
+    @RequestMapping(path = "/add_auction", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+    public ItemAddResponseDto register(@RequestBody ItemAddRequestDto itemAddRequestDto) throws Exception {
+
+
+        // Create Item
+//        Item item = UserMapper.registerRequestToUser(userSignUpRequestDto);
+        Item new_item = ItemMapper.registerRequestToItem(itemAddRequestDto);
+//        userRepository.save(new_user);
+        itemRepository.save(new_item);
+        System.out.println(new_item);
+        // Create dummy response
+        long i = 1;
+        ItemAddResponseDto itemAddResponseDto = new ItemAddResponseDto();
+        itemAddResponseDto.setItemId(i);
+
+        return itemAddResponseDto; // TODO: Return something meaningful.
+
+    }
+
+    @RequestMapping(path="/auctions_list", method = RequestMethod.GET, produces = "application/json")
+    public List<ItemDto> auctions_list() throws Exception {
+
+        List<Item> items = itemRepository.findByStartDate(null);
+        // TODO: We need to add the list of categories associated with every item somehow somewhere. Maybe even in a previeous step (fetch it automaticaly using a `find` function)
+
+        return convertToItemDTOs(items);
+    }
+
+
+//    @RequestMapping(path="/get_auction/{itemId}", method = RequestMethod.GET, produces = "application/json")
+//    public UserDto get_user(@PathVariable int userId) throws Exception {
+//        Users user = userRepository.findUserByUserId(userId);
+//
+//        return UserMapper.registerUsersToUser(user);
+//    }
 
     @ExceptionHandler(Exception.class)
     public void notFound(HttpServletResponse e) throws Exception {
--- src/main/java/com/mappers/ItemMapper.java
+++ src/main/java/com/mappers/ItemMapper.java
@@ -1,5 +1,6 @@
 package com.mappers;
 
+import com.dto.ItemAddRequestDto;
 import com.dto.ItemDto;
 import com.entity.Item;
 
@@ -7,6 +8,34 @@ import com.entity.Item;
  * Created by panos on 8/1/16.
  */
 public class ItemMapper {
+
+
+    public static Item registerRequestToItem(ItemAddRequestDto itemAddRequestDto) {
+
+        if (itemAddRequestDto == null)
+            return null;
+
+        Item  item = new Item();
+
+        item.setBuyPrice(itemAddRequestDto.getBuyPrice());
+        item.setCountry(itemAddRequestDto.getCountry());
+        item.setCreatedDate(itemAddRequestDto.getCreatedDate());
+        item.setCurrently(itemAddRequestDto.getFirstBid());
+        item.setDescription(itemAddRequestDto.getDescription());
+        item.setEndDate(itemAddRequestDto.getEndDate());
+        item.setFirstBid(itemAddRequestDto.getFirstBid());
+        item.setLatitude(itemAddRequestDto.getLatitude());
+        item.setLongitude(itemAddRequestDto.getLongitude());
+        item.setName(itemAddRequestDto.getName());
+        item.setNumberOfBids(0);
+        item.setSellerId(itemAddRequestDto.getSellerId());
+        item.setStartDate(itemAddRequestDto.getStartDate());
+
+        // TODO: WE NEED TO ADD CATEGORIES TO THE ITEM
+
+        return item;
+    }
+
 
     public static ItemDto registerItemToItem(Item item) {
 
