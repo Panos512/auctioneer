43540f2a56999d9de4434af45bc335e4c2aa0735
Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>
2016-09-21 22:44:59 +0200

    * Adds auctions search interface. * Adds name and description text-search. * Adds bid range-search.

--- src/main/resources/static/js/controllers/auctions_controller.js
+++ src/main/resources/static/js/controllers/auctions_controller.js
@@ -7,6 +7,17 @@ app.controller('AuctionsController', ['$scope', 'RequestServices', function($sco
         $('#grid').click(function(event){event.preventDefault();$('#products .item').removeClass('list-group-item');$('#products .item').addClass('grid-group-item');});
     });
 
+    $scope.byRange = function (fieldName, minValue, maxValue) {
+        console.log(fieldName, minValue, maxValue);
+        if (minValue === undefined || minValue == null) minValue = Number.MIN_VALUE;
+        if (maxValue === undefined || maxValue == null) maxValue = Number.MAX_VALUE;
+
+        return function predicateFunc(item) {
+            console.log(item);
+            return minValue <= item[fieldName] && item[fieldName] <= maxValue;
+        };
+    };
+
     RequestServices.auctions_list().then(function (response){
         console.log(response);
         $scope.auctions = response;
