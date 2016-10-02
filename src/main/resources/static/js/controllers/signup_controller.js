aedaa9cfc580503bbacf6aa32ab5b107b600987f
Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>
2016-10-02 17:32:32 +0200

    * Enables signup validations. * Adds geocoding field on signup.

--- src/main/resources/static/js/controllers/signup_controller.js
+++ src/main/resources/static/js/controllers/signup_controller.js
@@ -19,6 +19,12 @@ app.controller('SignUpController', ['$scope', '$location','RequestServices' , 's
         gender: ''
     };
 
+    $scope.options = {
+        types: 'geocode',
+        watchEnter: true,
+        country: 'gr'
+    };
+
 
     $scope.vaildateEmail = function(email) {
         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -40,71 +46,63 @@ app.controller('SignUpController', ['$scope', '$location','RequestServices' , 's
 
 
         //UNCOMMENT FOLLOWING LINES TO ENABLE VALIDATIONS
-        // if ($scope.credentials.username === '') {
-        //     $scope.username_missing_error = 'set';
-        //     miss_error = true;
-        // }
-        // if ($scope.credentials.first_name === '') {
-        //     $scope.first_name_missing_error = 'set';
-        //     miss_error = true;
-        // }
-        // if ($scope.credentials.last_name === '') {
-        //     $scope.last_name_missing_error = 'set';
-        //     miss_error = true;
-        // }
-        // if ($scope.credentials.password === '') {
-        //     $scope.password_missing_error = 'set';
-        //     miss_error = true;
-        // }
-        // if ($scope.credentials.rep_password === '') {
-        //     $scope.rep_password_missing_error = 'set';
-        //     miss_error = true;
-        // }
-        // if ($scope.credentials.mail === '') {
-        //     $scope.mail_missing_error = 'set';
-        //     miss_error = true;
-        // }
-        // if ($scope.credentials.phone_number === '') {
-        //     $scope.phone_number_missing_error = 'set';
-        //     miss_error = true;
-        // }
-        // if ($scope.credentials.latitude === '') {
-        //     $scope.latitude_missing_error = 'set';
-        //     miss_error = true;
-        // }
-        // if ($scope.credentials.longitude === '') {
-        //     $scope.longitude_missing_error = 'set';
-        //     miss_error = true;
-        // }
-        // if ($scope.credentials.afm === '') {
-        //     $scope.afm_missing_error = 'set';
-        //     miss_error = true;
-        // }
-        // if ($scope.credentials.address === '') {
-        //     $scope.address_missing_error = 'set';
-        //     miss_error = true;
-        // }
-        //
-        //
-        // if ($scope.credentials.phone_number.length < 10 && miss_error == false) {
-        //     $scope.phone_number_error = 'set';
-        //     error = true;
-        // }
-        //
-        // if ($scope.credentials.password.length < 5 && miss_error == false) { // TODO: Should prevent pass_error to be sent.p
-        //     $scope.pass_len_error = 'set';
-        //     error = true;
-        // }
-        //
-        // if ($scope.credentials.password !== $scope.credentials.rep_password && miss_error == false) {
-        //     $scope.pass_error = 'set';
-        //     error = true;
-        // }
-        //
-        // if ($scope.vaildateEmail($scope.credentials.mail) != true && miss_error == false) {
-        //     $scope.mail_error = 'set';
-        //     error=true;
-        // }
+        if ($scope.credentials.username === '') {
+            $scope.username_missing_error = 'set';
+            miss_error = true;
+        }
+        if ($scope.credentials.first_name === '') {
+            $scope.first_name_missing_error = 'set';
+            miss_error = true;
+        }
+        if ($scope.credentials.last_name === '') {
+            $scope.last_name_missing_error = 'set';
+            miss_error = true;
+        }
+        if ($scope.credentials.password === '') {
+            $scope.password_missing_error = 'set';
+            miss_error = true;
+        }
+        if ($scope.credentials.rep_password === '') {
+            $scope.rep_password_missing_error = 'set';
+            miss_error = true;
+        }
+        if ($scope.credentials.mail === '') {
+            $scope.mail_missing_error = 'set';
+            miss_error = true;
+        }
+        if ($scope.credentials.phone_number === '') {
+            $scope.phone_number_missing_error = 'set';
+            miss_error = true;
+        }
+        if ($scope.credentials.afm === '') {
+            $scope.afm_missing_error = 'set';
+            miss_error = true;
+        }
+        if ($scope.geoloc === '') {
+            $scope.address_missing_error = 'set';
+            miss_error = true;
+        }
+
+
+        if ($scope.credentials.phone_number.length < 10 && miss_error == false) {
+            $scope.phone_number_error = 'set';
+            error = true;
+        }
+
+        if ($scope.credentials.password.length < 5 && miss_error == false) { // TODO: Should prevent pass_error to be sent.p
+            $scope.pass_len_error = 'set';
+            error = true;
+        }
+
+        if ($scope.credentials.password !== $scope.credentials.rep_password && miss_error == false) {
+            $scope.pass_error = 'set';
+            error = true;
+        }
+
+        if ($scope.vaildateEmail($scope.credentials.mail) != true && miss_error == false) {
+            $scope.mail_error = 'set';
+            error=true;
+        }
 
 
         var request = {
@@ -118,10 +116,15 @@ app.controller('SignUpController', ['$scope', '$location','RequestServices' , 's
             latitude: $scope.credentials.latitude,
             longitude: $scope.credentials.longitude,
             afm: $scope.credentials.afm,
-            address: $scope.credentials.address,
+            address: $scope.geoloc,
             role: 'user',
             gender: $scope.credentials.gender
         };
+
+        var address = $scope.details.geometry.location;
+
+        request.latitude = address.lat();
+        request.longitude = address.lng();
 
 
         if (error ||  miss_error) return;
